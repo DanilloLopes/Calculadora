ddfa77aa4d2fc50e91ffd81e5648858c6af06afa
Danillo Lopes <128190826+DanilloLopes@users.noreply.github.com>
2023-07-26 20:29:41 -0300

    Add files via upload

---
+++ CalculadoraIMC/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace CalculadoraIMC
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelInferior = new System.Windows.Forms.Panel();
+            this.btnNumero7 = new System.Windows.Forms.Button();
+            this.panelSuperior = new System.Windows.Forms.Panel();
+            this.btnNumero8 = new System.Windows.Forms.Button();
+            this.btnNumero9 = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnVirgula = new System.Windows.Forms.Button();
+            this.btnNumero0 = new System.Windows.Forms.Button();
+            this.btnNumero6 = new System.Windows.Forms.Button();
+            this.btnNumero5 = new System.Windows.Forms.Button();
+            this.btnNumero4 = new System.Windows.Forms.Button();
+            this.btnNumero3 = new System.Windows.Forms.Button();
+            this.btnNumero2 = new System.Windows.Forms.Button();
+            this.btnNumero1 = new System.Windows.Forms.Button();
+            this.bntCalcularIndice = new System.Windows.Forms.Button();
+            this.panelInferior.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelInferior
+            // 
+            this.panelInferior.BackColor = System.Drawing.Color.Black;
+            this.panelInferior.Controls.Add(this.bntCalcularIndice);
+            this.panelInferior.Controls.Add(this.btnNumero3);
+            this.panelInferior.Controls.Add(this.btnNumero6);
+            this.panelInferior.Controls.Add(this.btnVirgula);
+            this.panelInferior.Controls.Add(this.btnNumero0);
+            this.panelInferior.Controls.Add(this.btnNumero4);
+            this.panelInferior.Controls.Add(this.btnNumero1);
+            this.panelInferior.Controls.Add(this.btnDelete);
+            this.panelInferior.Controls.Add(this.btnNumero2);
+            this.panelInferior.Controls.Add(this.btnNumero5);
+            this.panelInferior.Controls.Add(this.btnNumero9);
+            this.panelInferior.Controls.Add(this.btnNumero8);
+            this.panelInferior.Controls.Add(this.btnNumero7);
+            this.panelInferior.Location = new System.Drawing.Point(12, 208);
+            this.panelInferior.Name = "panelInferior";
+            this.panelInferior.Size = new System.Drawing.Size(377, 301);
+            this.panelInferior.TabIndex = 0;
+            // 
+            // btnNumero7
+            // 
+            this.btnNumero7.BackColor = System.Drawing.Color.Gold;
+            this.btnNumero7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnNumero7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNumero7.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNumero7.Location = new System.Drawing.Point(34, 19);
+            this.btnNumero7.Name = "btnNumero7";
+            this.btnNumero7.Size = new System.Drawing.Size(99, 52);
+            this.btnNumero7.TabIndex = 3;
+            this.btnNumero7.Text = "7";
+            this.btnNumero7.UseVisualStyleBackColor = false;
+            // 
+            // panelSuperior
+            // 
+            this.panelSuperior.Location = new System.Drawing.Point(12, 13);
+            this.panelSuperior.Name = "panelSuperior";
+            this.panelSuperior.Size = new System.Drawing.Size(377, 189);
+            this.panelSuperior.TabIndex = 1;
+            // 
+            // btnNumero8
+            // 
+            this.btnNumero8.BackColor = System.Drawing.Color.Gold;
+            this.btnNumero8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnNumero8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNumero8.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNumero8.Location = new System.Drawing.Point(139, 19);
+            this.btnNumero8.Name = "btnNumero8";
+            this.btnNumero8.Size = new System.Drawing.Size(99, 52);
+            this.btnNumero8.TabIndex = 4;
+            this.btnNumero8.Text = "8";
+            this.btnNumero8.UseVisualStyleBackColor = false;
+            // 
+            // btnNumero9
+            // 
+            this.btnNumero9.BackColor = System.Drawing.Color.Gold;
+            this.btnNumero9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnNumero9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNumero9.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNumero9.Location = new System.Drawing.Point(244, 19);
+            this.btnNumero9.Name = "btnNumero9";
+            this.btnNumero9.Size = new System.Drawing.Size(99, 52);
+            this.btnNumero9.TabIndex = 12;
+            this.btnNumero9.Text = "9";
+            this.btnNumero9.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Gold;
+            this.btnDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(244, 193);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(99, 52);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Del";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnVirgula
+            // 
+            this.btnVirgula.BackColor = System.Drawing.Color.Gold;
+            this.btnVirgula.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnVirgula.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVirgula.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVirgula.Location = new System.Drawing.Point(34, 193);
+            this.btnVirgula.Name = "btnVirgula";
+            this.btnVirgula.Size = new System.Drawing.Size(99, 52);
+            this.btnVirgula.TabIndex = 14;
+            this.btnVirgula.Text = ",";
+            this.btnVirgula.UseVisualStyleBackColor = false;
+            // 
+            // btnNumero0
+            // 
+            this.btnNumero0.BackColor = System.Drawing.Color.Gold;
+            this.btnNumero0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnNumero0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNumero0.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNumero0.Location = new System.Drawing.Point(139, 193);
+            this.btnNumero0.Name = "btnNumero0";
+            this.btnNumero0.Size = new System.Drawing.Size(99, 52);
+            this.btnNumero0.TabIndex = 15;
+            this.btnNumero0.Text = "0";
+            this.btnNumero0.UseVisualStyleBackColor = false;
+            // 
+            // btnNumero6
+            // 
+            this.btnNumero6.BackColor = System.Drawing.Color.Gold;
+            this.btnNumero6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnNumero6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNumero6.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNumero6.Location = new System.Drawing.Point(244, 77);
+            this.btnNumero6.Name = "btnNumero6";
+            this.btnNumero6.Size = new System.Drawing.Size(99, 52);
+            this.btnNumero6.TabIndex = 16;
+            this.btnNumero6.Text = "6";
+            this.btnNumero6.UseVisualStyleBackColor = false;
+            // 
+            // btnNumero5
+            // 
+            this.btnNumero5.BackColor = System.Drawing.Color.Gold;
+            this.btnNumero5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnNumero5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNumero5.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNumero5.Location = new System.Drawing.Point(139, 77);
+            this.btnNumero5.Name = "btnNumero5";
+            this.btnNumero5.Size = new System.Drawing.Size(99, 52);
+            this.btnNumero5.TabIndex = 17;
+            this.btnNumero5.Text = "5";
+            this.btnNumero5.UseVisualStyleBackColor = false;
+            // 
+            // btnNumero4
+            // 
+            this.btnNumero4.BackColor = System.Drawing.Color.Gold;
+            this.btnNumero4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnNumero4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNumero4.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNumero4.Location = new System.Drawing.Point(34, 77);
+            this.btnNumero4.Name = "btnNumero4";
+            this.btnNumero4.Size = new System.Drawing.Size(99, 52);
+            this.btnNumero4.TabIndex = 18;
+            this.btnNumero4.Text = "4";
+            this.btnNumero4.UseVisualStyleBackColor = false;
+            // 
+            // btnNumero3
+            // 
+            this.btnNumero3.BackColor = System.Drawing.Color.Gold;
+            this.btnNumero3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnNumero3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNumero3.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNumero3.Location = new System.Drawing.Point(244, 135);
+            this.btnNumero3.Name = "btnNumero3";
+            this.btnNumero3.Size = new System.Drawing.Size(99, 52);
+            this.btnNumero3.TabIndex = 19;
+            this.btnNumero3.Text = "3";
+            this.btnNumero3.UseVisualStyleBackColor = false;
+            // 
+            // btnNumero2
+            // 
+            this.btnNumero2.BackColor = System.Drawing.Color.Gold;
+            this.btnNumero2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnNumero2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNumero2.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNumero2.Location = new System.Drawing.Point(139, 135);
+            this.btnNumero2.Name = "btnNumero2";
+            this.btnNumero2.Size = new System.Drawing.Size(99, 52);
+            this.btnNumero2.TabIndex = 20;
+            this.btnNumero2.Text = "2";
+            this.btnNumero2.UseVisualStyleBackColor = false;
+            // 
+            // btnNumero1
+            // 
+            this.btnNumero1.BackColor = System.Drawing.Color.Gold;
+            this.btnNumero1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnNumero1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNumero1.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNumero1.Location = new System.Drawing.Point(34, 135);
+            this.btnNumero1.Name = "btnNumero1";
+            this.btnNumero1.Size = new System.Drawing.Size(99, 52);
+            this.btnNumero1.TabIndex = 21;
+            this.btnNumero1.Text = "1";
+            this.btnNumero1.UseVisualStyleBackColor = false;
+            // 
+            // bntCalcularIndice
+            // 
+            this.bntCalcularIndice.BackColor = System.Drawing.Color.Gold;
+            this.bntCalcularIndice.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.bntCalcularIndice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bntCalcularIndice.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntCalcularIndice.Location = new System.Drawing.Point(34, 251);
+            this.bntCalcularIndice.Name = "bntCalcularIndice";
+            this.bntCalcularIndice.Size = new System.Drawing.Size(309, 30);
+            this.bntCalcularIndice.TabIndex = 15;
+            this.bntCalcularIndice.Text = "Calcular Índice de Massa Corporal";
+            this.bntCalcularIndice.UseVisualStyleBackColor = false;
+            // 
+            // frmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(401, 521);
+            this.Controls.Add(this.panelSuperior);
+            this.Controls.Add(this.panelInferior);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmCalculadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.panelInferior.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelInferior;
+        private System.Windows.Forms.Panel panelSuperior;
+        private System.Windows.Forms.Button btnNumero7;
+        private System.Windows.Forms.Button btnNumero1;
+        private System.Windows.Forms.Button btnNumero2;
+        private System.Windows.Forms.Button btnNumero3;
+        private System.Windows.Forms.Button btnNumero4;
+        private System.Windows.Forms.Button btnNumero5;
+        private System.Windows.Forms.Button btnNumero6;
+        private System.Windows.Forms.Button btnNumero0;
+        private System.Windows.Forms.Button btnVirgula;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnNumero9;
+        private System.Windows.Forms.Button btnNumero8;
+        private System.Windows.Forms.Button bntCalcularIndice;
+    }
+}
+
